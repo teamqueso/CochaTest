76a45afd6ed94bb0c790a134c577a2ec5de8a585
n0g4y0 <n0g4y0@gmail.com>
2019-09-12 11:18:08 -0400

    tema #1, primera APP

--- app/src/main/java/io/github/n0g4y0/cochatest/MainActivity.kt
+++ app/src/main/java/io/github/n0g4y0/cochatest/MainActivity.kt
@@ -2,11 +2,32 @@ package io.github.n0g4y0.cochatest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var botonVerdad : Button
+    private lateinit var botonFalso : Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        botonVerdad = findViewById(R.id.verdad_button)
+        botonFalso = findViewById(R.id.falso_button)
+
+        botonVerdad.setOnClickListener {
+            val muestraMensaje = Toast.makeText(this, R.string.correcto_toast, Toast.LENGTH_SHORT)
+            muestraMensaje.show()
+        }
+
+        botonFalso.setOnClickListener {
+
+            val muestraMensaje = Toast.makeText(this, R.string.incorrecto_toast, Toast.LENGTH_SHORT)
+            muestraMensaje.show()
+        }
     }
 }
