4882f986a8641baa1cbe7e164d5e20ad7719fc27
n0g4y0 <n0g4y0@gmail.com>
2019-09-12 10:53:42 -0400

    codigo base

---
+++ app/src/main/java/io/github/n0g4y0/cochatest/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.n0g4y0.cochatest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
