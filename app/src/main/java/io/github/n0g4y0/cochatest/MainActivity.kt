e2202eb528f2e0b6644f6046d394782a85ae7b49
n0g4y0 <n0g4y0@gmail.com>
2019-09-12 12:43:02 -0400

    tema 2: se agregaron preguntas, una clase PREGUNTAS, y la imagen de la flecha izquierda y derecha

--- app/src/main/java/io/github/n0g4y0/cochatest/MainActivity.kt
+++ app/src/main/java/io/github/n0g4y0/cochatest/MainActivity.kt
@@ -5,12 +5,27 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var botonVerdad : Button
     private lateinit var botonFalso : Button
+    private lateinit var botonSiguiente : Button
 
+
+    private var indiceActual = 0
+
+    private val bancoPreguntas = listOf(
+        Pregunta(R.string.question_1, false),
+        Pregunta(R.string.question_2,false),
+        Pregunta(R.string.question_3,true),
+        Pregunta(R.string.question_4,false),
+        Pregunta(R.string.question_5,true),
+        Pregunta(R.string.question_6,false),
+        Pregunta(R.string.question_7,true),
+        Pregunta(R.string.question_8,false)
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,16 +33,41 @@ class MainActivity : AppCompatActivity() {
 
         botonVerdad = findViewById(R.id.verdad_button)
         botonFalso = findViewById(R.id.falso_button)
+        botonSiguiente = findViewById(R.id.siguiente_button)
 
         botonVerdad.setOnClickListener {view : View ->
-            val muestraMensaje = Toast.makeText(this, R.string.correcto_toast, Toast.LENGTH_SHORT)
-            muestraMensaje.show()
+            verificarRespuesta(true)
         }
 
         botonFalso.setOnClickListener {view : View ->
 
-            val muestraMensaje = Toast.makeText(this, R.string.incorrecto_toast, Toast.LENGTH_SHORT)
-            muestraMensaje.show()
+            verificarRespuesta(false)
         }
+
+        botonSiguiente.setOnClickListener {
+                indiceActual = (indiceActual + 1) % bancoPreguntas.size
+                actualizarPregunta()
+        }
+
+        actualizarPregunta()
+    }
+
+    private fun actualizarPregunta(){
+
+        val textoPreguntaID = bancoPreguntas[indiceActual].idTexto
+        pregunta_text_view.setText(textoPreguntaID)
+
+    }
+
+    private fun verificarRespuesta(respuestaUsuario: Boolean){
+
+            val respuestaCorrecta = bancoPreguntas[indiceActual].respuesta
+            val mostrarMensajeID =  if (respuestaUsuario == respuestaCorrecta){
+                "Correcto"
+            } else {
+                "Incorrecto"
+            }
+            Toast.makeText(this,mostrarMensajeID,Toast.LENGTH_SHORT).show()
+
     }
 }
---
+++ app/src/main/java/io/github/n0g4y0/cochatest/Pregunta.kt
@@ -0,0 +1,5 @@
+package io.github.n0g4y0.cochatest
+
+import android.support.annotation.StringRes
+
+data class Pregunta(@StringRes val idTexto : Int, val respuesta: Boolean)
