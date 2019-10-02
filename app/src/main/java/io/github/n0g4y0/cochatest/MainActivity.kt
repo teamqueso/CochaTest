133ca3c845628790f8564790cd5b377101c502b4
n0g4y0 <n0g4y0@gmail.com>
2019-10-02 16:17:47 -0400

    solo layout, faltan variables y funciones

--- app/src/main/java/io/github/n0g4y0/cochatest/MainActivity.kt
+++ app/src/main/java/io/github/n0g4y0/cochatest/MainActivity.kt
@@ -8,26 +8,8 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var botonVerdad : Button
-    private lateinit var botonFalso : Button
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        botonVerdad = findViewById(R.id.verdad_button)
-        botonFalso = findViewById(R.id.falso_button)
-
-        botonVerdad.setOnClickListener {view : View ->
-            val muestraMensaje = Toast.makeText(this, R.string.correcto_toast, Toast.LENGTH_SHORT)
-            muestraMensaje.show()
-        }
-
-        botonFalso.setOnClickListener {view : View ->
-
-            val muestraMensaje = Toast.makeText(this, R.string.incorrecto_toast, Toast.LENGTH_SHORT)
-            muestraMensaje.show()
-        }
     }
 }
